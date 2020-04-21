f37bbe265f89de417f3533376b7ca68d504f16e6
Ameya Mhaisekar <amhaisekar@vmware.com>
2020-04-21 00:04:57 -0700

    New Lists package..Come back for this later

---
+++ lists/target/debug/deps/lists-1094129e402f807e.d
@@ -0,0 +1,8 @@
+/Users/amhaisekar/strommer-git/rust/lists/target/debug/deps/lists-1094129e402f807e.rmeta: src/lib.rs src/first.rs
+
+/Users/amhaisekar/strommer-git/rust/lists/target/debug/deps/liblists-1094129e402f807e.rlib: src/lib.rs src/first.rs
+
+/Users/amhaisekar/strommer-git/rust/lists/target/debug/deps/lists-1094129e402f807e.d: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
---
+++ lists/target/debug/liblists.d
@@ -0,0 +1,1 @@
+/Users/amhaisekar/strommer-git/rust/lists/target/debug/liblists.rlib: /Users/amhaisekar/strommer-git/rust/lists/src/first.rs /Users/amhaisekar/strommer-git/rust/lists/src/lib.rs
